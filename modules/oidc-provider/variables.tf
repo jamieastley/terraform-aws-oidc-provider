ba6b35220a1267f0d717b4aec9248d184d3cf86d
jamieastley <6836556+jamieastley@users.noreply.github.com>
2024-11-14 17:34:28 +1100

    terraform fmt

--- modules/oidc-provider/variables.tf
+++ modules/oidc-provider/variables.tf
@@ -6,17 +6,17 @@ variable "identity_provider_url" {
 
 variable "identity_provider_client_id_list" {
   description = "The list of client IDs"
-  type = list(string)
-  default = ["sts.amazonaws.com"]
+  type        = list(string)
+  default     = ["sts.amazonaws.com"]
 }
 
 variable "oidc_provider_thumbprint_list" {
   description = "(Optional) A list of server certificate thumbprints for the OpenID Connect (OIDC) identity provider"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "resource_tags" {
   description = "A map of tags to add to the resources"
-  type = map(string)
+  type        = map(string)
 }

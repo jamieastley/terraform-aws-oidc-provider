8efcc85e7275e24254d0ee9e3e115bc279e81f77
jamieastley <6836556+jamieastley@users.noreply.github.com>
2026-02-10 23:24:31 +1100

    feat!: Remove resource_tags variable (#120)
    Removed the `resource_tags` variable to allow consumers to set their own
    tags via the `provider` block.

--- modules/oidc-provider/main.tf
+++ modules/oidc-provider/main.tf
@@ -13,6 +13,4 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list = var.identity_provider_client_id_list
 
   thumbprint_list = var.oidc_provider_thumbprint_list
-
-  tags = var.resource_tags
 }
